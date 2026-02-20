c88b71bd042a337e43f1797328246878c10090c7
Evan Diercks <evantd2005@gmail.com>
2026-02-19 18:20:24 -0600

    added dummy functionality when clicked to sidebar

--- nonogram_project/gui.py
+++ nonogram_project/gui.py
@@ -4,15 +4,23 @@ root = tk.Tk()
 root.title("nonogram")
 root.geometry("500x500")
 
-
-
+# =============================================================================
+# Make sure that all functions pass event as an argument
+# and that all .binds do not have () at the end!!!!
+# =============================================================================
+def test(event):
+    print("hello world!")
 
 # Create Sidebar
 
 root.update()
-sidebar = tk.Frame(root, width=100, bg="red", height= root.winfo_height())
+sidebar = tk.Frame(root, width=300, bg="red", height= root.winfo_height())
 sidebar.grid()
+
 # Create Input for length/width to allow selection of nongram size
+
+root.update()
+sidebar.bind("<Button>", test)
 
 nrow = tk.IntVar()
 nrow_input = tk.Entry(sidebar, textvariable=nrow)
